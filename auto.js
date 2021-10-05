05ff5db398b4771ec8681613e5d94a841c758d53
Jordan Harband <ljharb@gmail.com>
2021-10-04 21:18:26 -0700

    [Tests] add `@es-shims/api`

--- auto.js
+++ auto.js
@@ -1,10 +1,3 @@
 'use strict';
 
-require('./Error/auto');
-require('./AggregateError/auto');
-require('./EvalError/auto');
-require('./RangeError/auto');
-require('./ReferenceError/auto');
-require('./SyntaxError/auto');
-require('./TypeError/auto');
-require('./URIError/auto');
+require('./shim')();
