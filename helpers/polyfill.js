6924f485637d9d535e19e7bf36f9e3d9e189d5af
Jordan Harband <ljharb@gmail.com>
2022-01-18 16:17:54 -0800

    [patch] add comment for v8 bug

--- helpers/polyfill.js
+++ helpers/polyfill.js
@@ -4,7 +4,11 @@ module.exports = function polyfillHelper(C, implementation) {
 	return function polyfill() {
 		var options = { cause: { sentinel: true } };
 		var e = new C('a', options);
-		if ('cause' in e && !('cause' in C.prototype) && e.cause === options.cause) {
+		if (
+			'cause' in e
+			&& !('cause' in C.prototype) // https://bugs.chromium.org/p/v8/issues/detail?id=12006
+			&& e.cause === options.cause
+		) {
 			return C;
 		}
 		return implementation;
